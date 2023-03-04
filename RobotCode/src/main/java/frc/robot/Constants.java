13517feb01eac54232d7901cfcbb95ef8056b4a1
Bushsbakedbean <114026987+Bushsbakedbean@users.noreply.github.com>
2023-03-04 16:21:35 -0500

    Adjustments

--- RobotCode/src/main/java/frc/robot/Constants.java
+++ RobotCode/src/main/java/frc/robot/Constants.java
@@ -91,7 +91,7 @@ public final class Constants {
     //public static final double maxGrabEncoderValue = 80;
     //public static final double minGrabEncoderValue = -47;
     public static final double maxGrabEncoderValue = .7;
-    public static final double minGrabEncoderValue = .45;
+    public static final double minGrabEncoderValue = .40;
 
     public static final int extensionEncoderDIO = 9;
  
